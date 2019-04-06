562ab8519281e4e9b203d31db0ce9f9f1beac6d0
Jakub Scholz <www@scholzj.com>
2019-04-07 00:07:28 +0200

    Update for alpine image

--- kafka-consumer/scripts/run.sh
+++ kafka-consumer/scripts/run.sh
@@ -1,51 +1,46 @@
-#!/bin/bash
+#!/bin/sh
 set +x
 
-# Parameters:
-# $1: Path to the new truststore
-# $2: Truststore password
-# $3: Public key to be imported
-# $4: Alias of the certificate
-function create_truststore {
-   keytool -keystore $1 -storepass $2 -noprompt -alias $4 -import -file $3 -storetype PKCS12
-}
-
-# Parameters:
-# $1: Path to the new keystore
-# $2: Truststore password
-# $3: Public key to be imported
-# $4: Private key to be imported
-# $5: Alias of the certificate
-function create_keystore {
-   RANDFILE=/tmp/.rnd openssl pkcs12 -export -in $3 -inkey $4 -name $HOSTNAME -password pass:$2 -out $1
-}
-
-if [ "$CA_CRT" ];
+if [[ "$TRUSTSTORE_FILE" && "$TRUSTSTORE_PASSWORD_FILE" ]];
 then
-    echo "Preparing truststore"
-    export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SECURITY_PROTOCOL=SSL
-    export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_TRUSTSTORE_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
-    echo "$CA_CRT" > /tmp/ca.crt
-    create_truststore /tmp/truststore.p12 $SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_TRUSTSTORE_PASSWORD /tmp/ca.crt ca
-    export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_TRUSTSTORE_TYPE=PKCS12
-    export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_TRUSTSTORE_LOCATION=/tmp/truststore.p12
+    if [[ -f $TRUSTSTORE_FILE && -f $TRUSTSTORE_PASSWORD_FILE ]];
+    then
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SECURITY_PROTOCOL=SSL
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_TRUSTSTORE_PASSWORD="$(cat $TRUSTSTORE_PASSWORD_FILE)"
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_TRUSTSTORE_TYPE=PKCS12
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_TRUSTSTORE_LOCATION=$TRUSTSTORE_FILE
+
+        # Have to set the KEYSTORE config to dummy values (based on truststore)
+        # Otherwise Kafka will complain about the keystore values being set
+        # This is a Quarkus limitation of how it deals with configuration :-(
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_KEYSTORE_PASSWORD="$(cat $TRUSTSTORE_PASSWORD_FILE)"
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_KEYSTORE_TYPE=PKCS12
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_KEYSTORE_LOCATION=$TRUSTSTORE_FILE
+    fi
 fi
 
-if [[ "$USER_CRT" && "$USER_KEY" ]];
+if [[ "$KEYSTORE_FILE" && "$KEYSTORE_PASSWORD_FILE" ]];
 then
-    echo "Preparing keystore"
-    export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SECURITY_PROTOCOL=SSL
-    export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_KEYSTORE_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
-    echo "$USER_CRT" > /tmp/user.crt
-    echo "$USER_KEY" > /tmp/user.key
-    create_keystore /tmp/keystore.p12 $SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_KEYSTORE_PASSWORD /tmp/user.crt /tmp/user.key /opt/kafka/cluster-certs/cluster-ca.crt $HOSTNAME
-    export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_KEYSTORE_TYPE=PKCS12
-    export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_KEYSTORE_LOCATION=/tmp/keystore.p12
+    if [[ -f $KEYSTORE_FILE && -f $KEYSTORE_PASSWORD_FILE ]];
+    then
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SECURITY_PROTOCOL=SSL
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_KEYSTORE_PASSWORD="$(cat $KEYSTORE_PASSWORD_FILE)"
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_KEYSTORE_TYPE=PKCS12
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SSL_KEYSTORE_LOCATION=$KEYSTORE_FILE
+    fi
 fi
 
 if [[ "$USERNAME" && "$PASSWORD" ]];
 then
     echo "Preparing JAAS configurtion"
+
+    if [[ "$SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SECURITY_PROTOCOL" && "$SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SECURITY_PROTOCOL" == "SSL" ]];
+    then
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SECURITY_PROTOCOL=SASL_SSL
+    else
+        export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SECURITY_PROTOCOL=SASL_PLAINTEXT
+    fi
+
     export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SASL_MECHANISM=SCRAM-SHA-512
     export SMALLRYE_MESSAGING_SOURCE_TEST_TOPIC_SASL_JAAS_CONFIG="org.apache.kafka.common.security.scram.ScramLoginModule required username=\"${USERNAME}\" password=\"${PASSWORD}\";"
 fi
